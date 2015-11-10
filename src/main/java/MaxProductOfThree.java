7a22643d83cca647899e4ad137644d155115732c
abudjakov <alexander.budjakov@gmail.com>
2015-11-10 01:18:07 +0000

    add MaxProductOfThree task

--- src/main/java/MaxProductOfThree.java
+++ src/main/java/MaxProductOfThree.java
@@ -1,2 +1,10 @@
+import java.util.Arrays;
+
 public class MaxProductOfThree {
+
+    public int solution(int[] A) {
+        Arrays.sort(A);
+        int N = A.length;
+        return Math.max(A[0] * A[1] * A[2], A[N - 1] * A[N - 2] * A[N - 3]);
+    }
 }
