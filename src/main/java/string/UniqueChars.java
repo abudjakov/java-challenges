f91f099235b12b27d0c2293269444f3138d55434
Alex <alexander.budjakov@gmail.com>
2019-01-13 22:16:57 +0000

    unique chars

---
+++ src/main/java/string/UniqueChars.java
@@ -0,0 +1,30 @@
+package string;
+
+// Solution for ASCII (2^7)
+public class UniqueChars {
+
+
+    static boolean isUnique(String str) {
+        if (str.length() > 128) {
+            return false;
+        }
+
+        int set = 0;
+        for (int i = 0; i < str.length(); i++) {
+            int code = str.charAt(i) - 'a';
+            if ((set & (1 << code)) > 0) {
+                return false;
+            }
+
+            set |= (1 << code);
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(UniqueChars.isUnique("abcdef"));
+        System.out.println(UniqueChars.isUnique("abcdbe"));
+
+    }
+}
