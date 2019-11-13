31c0e6c730caafb3c599e5c4f3f0246279d15c37
Alex <alexander.budjakov@gmail.com>
2019-11-13 22:43:57 +0000

    LCS

--- src/main/java/string/LongestCommonSubsequence.java
+++ src/main/java/string/LongestCommonSubsequence.java
@@ -29,11 +29,9 @@ public class LongestCommonSubsequence {
 
         int[][] arr = new int[n + 1][m + 1];
 
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= m; j++) {
-                if (i == 0 || j == 0) {
-                    arr[i][j] = 0;
-                } else if (a.charAt(i - 1) == b.charAt(j - 1)) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (a.charAt(i - 1) == b.charAt(j - 1)) {
                     arr[i][j] = 1 + arr[i - 1][j - 1];
                 } else {
                     arr[i][j] = Math.max(arr[i][j - 1], arr[i - 1][j]);
@@ -51,7 +49,7 @@ public class LongestCommonSubsequence {
         int ind = lcs - 1;
 
         while (i > 0 && j > 0) {
-            if (a.charAt(i - 1) == b.charAt(j -1)) {
+            if (a.charAt(i - 1) == b.charAt(j - 1)) {
                 chars[ind--] = a.charAt(i - 1);
                 i--;
                 j--;
@@ -60,8 +58,7 @@ public class LongestCommonSubsequence {
 
             if (arr[i - 1][j] > arr[i][j - 1]) {
                 i--;
-            }
-            else {
+            } else {
                 j--;
             }
         }
